c3c24438af972284c27e6702d218a5c600220168
Hemant Kumbhar <hemantkumbhar010@gmail.com>
2024-01-28 20:07:54 +0530

    adjust multer storage for cloudinary

--- backend/src/middlewares/cloudinary.middleware.ts
+++ backend/src/middlewares/cloudinary.middleware.ts
@@ -1,21 +1,12 @@
 import multer from "multer";
 import cloudinary from 'cloudinary';
 
-const memory_Storage = multer.memoryStorage();
-
-export const upload = multer({
-    storage: memory_Storage,
-    limits: {
-        fileSize: 5 * 1024 * 1024  //5MB
-    }
-})
-
-
 
 export const uploadMultipleImages = async (image_data: Express.Multer.File[]) => {
 
     try {
 
+       
         //UPLOADING IMAGES TO CLOUDINARY AND STORES ARRAY OF IMAGE URLS OF UPLADED IMAGES
         const uploadiPromises: Promise<string>[] = image_data.map(async (image) => {
 
@@ -32,9 +23,9 @@ export const uploadMultipleImages = async (image_data: Express.Multer.File[]) =>
 
         //WAITING TO UPLOAD ALL IMAGES AND GET A IMAGE URL ARRAY
         //BASICALLY TELLING THAT ANY IMAGE URL UPLOAD FAILED THEN THROW THE ERROR
-        const allImages = await Promise.all(uploadiPromises);
+        const imageUrls = await Promise.all(uploadiPromises);
 
-        return allImages;
+        return imageUrls;
     } catch (error) {
         throw new Error("Image upload failed! : "+ error);
     }
