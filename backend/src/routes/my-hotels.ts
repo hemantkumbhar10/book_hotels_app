45c4d76bc78b7253145ed44fe70b7fea46a67050
Hemant Kumbhar <hemantkumbhar010@gmail.com>
2024-01-31 00:58:07 +0530

    Add get hotels route

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -63,8 +63,13 @@ router.post(
     }
 );
 
-router.get('/', verifyToken, async(req:Request, res:Response)=>{
-    const hotels = await Hotel.find({userID:req.userID});
+router.get('/', verifyToken, async (req: Request, res: Response) => {
+    try {
+        const hotels = await Hotel.find({ userID: req.userID });
+        res.status(200).json(hotels);
+    } catch (e) {
+        res.status(500).json({ message: "Error fetching hotels!" });
+    }
 })
 
 export default router;
